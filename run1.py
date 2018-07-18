1cc53ba972c179ef8bfa6b71623e209d7b2ab2a6
Tony Yu <ziyitonyu@gmail.com>
2018-07-18 22:53:24 +0800

    my polyaxon demo 1.

--- run1.py
+++ run1.py
@@ -5,6 +5,9 @@ for i in range(startNum, endNum):
   sum = sum + i
 print sum
 
+# I can only get log output of sum ,
+# but can't find foo.txt file in persistence volume
+# claimed in polyaxon-config.yaml file
 fo = open("foo.txt", "w")
 fo.write(str(sum))
 fo.close
